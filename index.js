4130ce286d768901cbe7efa04816fc495ffbab32
FreedomWriter <natalienetworks@gmail.com>
2020-01-15 20:22:19 -0600

    mvp updated

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ server.get("/api/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(500).json({ success: false, err });
+      res
+        .status(500)
+        .json({ errorMessge: "The users information could not be retrieved." });
     });
 });
 
@@ -49,27 +51,35 @@ server.get("/api/users/:id", (req, res) => {
       if (user) {
         res.status(200).json({ success: true, user });
       } else {
-        res.status(404).json({ success: false, message: "id not found" });
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        });
       }
     })
     .catch(err => {
-      res.status(500).json({ success: false, err });
+      res.status(500).json({
+        errorMessage: "The user information could not be retrieved.",
+        err
+      });
     });
 });
 
 server.delete("/api/users/:id", (req, res) => {
   const { id } = req.params;
+
   db.remove(id)
     .then(deleted => {
       if (deleted) {
-        console.log(`Delete response`, deleted);
         res.status(204).end();
       } else {
-        req.status(404).json({ success: false, message: "id not found" });
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
       }
     })
     .catch(err => {
-      res.status(500).json({ success: false, err });
+      res.status(500).json({ errorMessage: "The user could not be removed" });
     });
 });
 
